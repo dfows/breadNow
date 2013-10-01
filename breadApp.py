832a623e6a9a3ad17b1476e9fe37b9d6afc1fcc6
Jessica Kwok <jessica@Pluot.local>
2013-10-01 09:50:52 -0400

    fixed urls

--- breadApp.py
+++ breadApp.py
@@ -1,30 +1,30 @@
 import os, datetime
 from flask import * #Flask,render_template,session,redirect,url_for
-#from pymongo import MongoClient
-#import breads
+from pymongo import MongoClient
+import breads
 
 app = Flask(__name__)
 app.secret_key = 'asdf12345'
 
-#client = MongoClient(os.getenv('MONGOHQ_URL'))
-#database = client.dailyBread
-#breadbox = breads.Breads(database)
+client = MongoClient(os.getenv('MONGOHQ_URL'))
+database = client.dailyBread
+breadbox = breads.Breads(database)
 
-@app.route('/bread')
-def select_bread():
+@app.route('/quantity')
+def select_quant():
   session['breading'] = True
-  bread_of_week = "sourdough baguette" #breadbox.get_bow()
+  bread_of_week = breadbox.get_bow()
   session['bread'] = bread_of_week
-  return render_template("quantity.html", flavorName = bread_of_week) #should be most recent entry in DB table "BREAD OF WEEK"
+  return render_template("quantity.html", flavorName = bread_of_week) 
 
-@app.route('/quantity/<q>')
-def select_quant(q):
+@app.route('/delivery-info/<q>')
+def enter_info(q):
   print "q is", q
   session['quantity'] = q
   return render_template("delivery-info.html")
 
-@app.route('/delivery-info', methods=['POST'])
-def enter_info():
+@app.route('/payment', methods=['POST'])
+def make_payment():
   error = None
   if request.method == 'POST': 
     session['address'] = request.form['delivery-address']
@@ -34,10 +34,10 @@ def enter_info():
     return render_template("payment.html")
   else:
     error = "You did not fill in all important fields"
-    return render_template("deliver-info.html",error=error)
+    return render_template("delivery-info.html",error=error)
 
-@app.route('/payment', methods=['POST'])
-def place_order():
+@app.route('/orderPlaced', methods=['POST'])
+def view_receipt():
   session['name'] = request.form['first-last-name']
   session['cc_num'] = request.form['cc-num']
   session['exp_date'] = request.form['exp-date']
@@ -45,7 +45,7 @@ def place_order():
   order_time = datetime.datetime.now()
   delivery_time = order_time+datetime.timedelta(minutes=30)
   deliverytime = delivery_time.strftime("%I:%M%p")
-  #breadbox.place_order(session['quantity'],session['name'],session['address'],session['phone'],session['email'],session['cc_num'],session['exp_date'],session['sec_code'])
+  breadbox.place_order(session['quantity'],session['name'],session['address'],session['phone'],session['email'],session['cc_num'],session['exp_date'],session['sec_code'])
   return render_template("orderPlaced.html", ordertime=deliverytime, name=session['name'], address=session['address'], email=session['email'], numLoaves=session['quantity'], bread=session['bread'])
 
 @app.route('/')
