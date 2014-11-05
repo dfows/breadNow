ad5e3db754e0ca56d37e8d70fc6ace6cce61454c
jkwok91 <0111291@gmail.com>
2014-11-05 00:11:30 -0800

    added stripe
    added phone number
    moved some code around
    replaced bad fn headers w better ones

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 var express = require('express');
 var path = require('path');
 var body = require('body/json');
+var stripe = require('stripe')("sk_test_IsqqoxGSOG3a4lUaplTaNwh5");
 
 var app = express();
-
-app.post('/placeOrder', function(req,res) {
-  // this is me putting an order into the db
-  // the full order
-  body(req,res,function(err,stuff) {
-    console.log(stuff);
-    res.end(JSON.stringify({status:200,responseText:stuff}));
-  });
-});
-
-app.get('/reviewOrder', function(req,res) {
-  // this is me returning the order for review
-  res.end(JSON.stringify({status:200,responseText:"tyhis is your order"}));
-});
 
 app.get('/getBread', function(req,res) {
   /*
@@ -31,6 +18,39 @@ app.get('/getBread', function(req,res) {
   var breadObj = {breadName:"sweet batard",breadPrice:5.5};
   res.end(JSON.stringify({status:200,responseText:breadObj}));
 });
+
+app.post('/charge', function(req,res) {
+  body(req,res,function(err,stuff) {
+    var breadPrice = parseInt(stuff.number)*parseFloat(stuff.price)*100;
+    console.log("charging u diz much",breadPrice);
+    stripe.charges.create({
+      amount: breadPrice,
+      currency: "usd",
+      card: stuff.cardToken, // obtained with Stripe.js
+      description: "Charge for bread"
+    }, function(err, charge) {
+      if (err) { res.send(err); }
+      placeOrder(stuff, function() {
+        console.log("WAAAZZAAA");
+        res.end(JSON.stringify({status:200,responseText:"yoooo"}));
+      });
+    });
+  });
+});
+
+function placeOrder(stuff, callback) {
+  // this is me putting an order into the db
+  // the full order
+  console.log("place order fn");
+  callback();
+  //db.orders.update(stuff,{upsert:true},function(err,res) {
+    // send response back to browza and be like, boom u placed an order
+  //});
+}
+
+function sendReceipt(stuff) {
+  // email receipt to a person
+}
 
 app.use(express.static(path.join(__dirname, 'public')));
 var port = process.env.PORT || 8002;
--- public/bread.js
+++ public/bread.js
@@ -9,16 +9,20 @@ if (!supports_html5_storage) {
   console.log("kill yourself; get HTML5 compatible browser");
 }
 
+Stripe.setPublishableKey('pk_test_XsWkTrpW4aniRIusa3BoQqb6');
 // global variable and references to DOM elements
 var pageNum = 0;
+var token;
 var $orderForm = $('#form');
 var $msg = $('#msg').hide();
+if (!localStorage['order']) { localStorage['order'] = JSON.stringify({}); }
 
 // form templates
 var form0 = generateOrderForm();
 var form1 = generateShippingForm();
 var form2 = generatePaymentForm();
-var formTemplates = [form0,form1,form2];
+var form3 = generateConfirmationPage();
+var formTemplates = [form0,form1,form2,form3];
 
 // display forms
 function prevPage() {
@@ -35,7 +39,6 @@ function showPage() {
 
 // save after changing forms
 function updateOrder(params) {
-  if (!localStorage['order']) { localStorage['order'] = JSON.stringify({}); }
   var order = JSON.parse(localStorage['order']);
   for (var key in params) {
     order[key] = params[key];
@@ -43,15 +46,26 @@ function updateOrder(params) {
   localStorage['order'] = JSON.stringify(order);
 }
 
+// review order
+function reviewOrder() {
+  return JSON.parse(localStorage['order']);
+}
+
 // submit order
 function finalizeOrder() {
+  var orderObj = {};
+  var order = JSON.parse(localStorage['order']);
+  for (var key in order) {
+    orderObj[key] = order[key];
+  }
+  orderObj['cardToken'] = token;
   $.ajax({
-    url: '/placeOrder',
-    data: localStorage['order'],
+    url: '/charge',
+    data: JSON.stringify(orderObj),
     type: 'POST',
     success: function() {
       console.log("yep");
-      $form.hide();
+      $orderForm.hide();
       $msg.text('Thanks for your order').show();
     },
     error: function() {
@@ -87,8 +101,9 @@ function generateOrderForm() {
 
   $continue[0].onclick = function() {
     var obj = {bread: $bread.text(),
-               price: $price.text(),
+               price: $price.text().slice(1), //dont want that dolla sign
                number: $number.val()};
+    console.log("THE PRICE IS RIGHT",obj.price);
     updateOrder(obj);
     nextPage();
   };
@@ -102,6 +117,7 @@ function generateShippingForm() {
   var strAddr1 = JSON.parse(localStorage['order']).strAddr1 || "";
   var strAddr2 = JSON.parse(localStorage['order']).strAddr2 || "";
   var zipC = JSON.parse(localStorage['order']).zip || "";
+  var phone = JSON.parse(localStorage['order']).phone || "";
 
   //generate a template for page 1 of form?
   var $form = $('<form>');
@@ -109,6 +125,7 @@ function generateShippingForm() {
   var $strAddr1 = $('<input class="input" type="text">').attr('name','customer-addr1').attr('placeholder','STREET ADDRESS 1').val(strAddr1);
   var $strAddr2 = $('<input class="input" type="text">').attr('name','customer-addr2').attr('placeholder','STREET ADDRESS 2 (optional)').val(strAddr2);
   var $zip = $('<input class="input" type="text">').attr('name','customer-zip').attr('placeholder','ZIP CODE').val(zipC);
+  var $phone = $('<input class="input" type="text">').attr('name','customer-phone').attr('placeholder','PHONE').val(phone);
   var $back = $('<a>').addClass('btn btn-lg').text('prev');
   var $continue = $('<a>').addClass('btn btn-lg').text('next');
 
@@ -119,32 +136,74 @@ function generateShippingForm() {
     var obj = {name: $name.val(),
                     strAddr1: $strAddr1.val(),
                     strAddr2: $strAddr2.val(),
-                    zip: $zip.val()};
+                    zip: $zip.val(),
+                    phone: $phone.val()};
     updateOrder(obj);
     nextPage();
   };
 
-  $form.append($name).append($strAddr1).append($strAddr2).append($zip).append($back).append($continue); 
+  $form.append($name).append($strAddr1).append($strAddr2).append($zip).append($phone).append($back).append($continue); 
   return $form;
 }
 
 function generatePaymentForm() {
   var $form = $('<form>');
+  var $cardNum = $('<input class="input" type="text" size="16">').attr('name','card-number').attr('placeholder','XXXXXXXXXXXXXXXX');
+  var $cardCVC = $('<input class="input" type="text">').attr('name','card-cvc').attr('placeholder','cvc');
+  var $expMo = $('<input class="input" type="text" size="2">').attr('name','card-exp-mo').attr('placeholder','MM');
+  var $expYr = $('<input class="input" type="text" size="2">').attr('name','card-exp-yr').attr('placeholder','YY');
   var $back = $('<a>').addClass('btn btn-lg').text('prev');
   var $continue = $('<a>').addClass('btn btn-lg').text('next');
+
+  /* TESTING */
+  $cardNum.val('4242424242424242');
+  $cardCVC.val('123');
+  $expMo.val('01');
+  $expYr.val('16');
   
   $back[0].onclick = function() {
     prevPage();
   };
   $continue[0].onclick = function() {
-    var obj = {name: $name.val(),
-                    strAddr1: $strAddr1.val(),
-                    strAddr2: $strAddr2.val(),
-                    zip: $zip.val()};
-    updateOrder(obj);
+    // stripe token to create and store cc token
+    Stripe.card.createToken({
+      number: $cardNum.val(),
+      cvc: $cardCVC.val(),
+      exp_month: $expMo.val(),
+      exp_year: $expYr.val()
+    }, function(err, response) {
+      if (err) { console.log(err); }
+      console.log("sup token",response.id);
+      token = response.id;
+    });
+    nextPage();
+  };
+
+  $form.append($cardNum).append($cardCVC).append($expMo).append($expYr).append($back).append($continue); 
+  return $form;
+}
+
+function generateConfirmationPage() {
+  var $form = $('<form>');
+  var $details = $('<div>');
+  var $back = $('<a>').addClass('btn btn-lg').text('prev');
+  var $continue = $('<a>').addClass('btn btn-lg').text('next');
+
+  var order = reviewOrder();
+  for (var key in order) {
+    var val = (key === 'price') ? '$'+order[key] : order[key];
+    var $field = $('<div>').addClass('order-detail').text(key+': '+val);
+    $details.append($field);
+  }
+  
+  $back[0].onclick = function() {
+    prevPage();
+  };
+  $continue[0].onclick = function() {
+    console.log("sup");
     finalizeOrder();
   };
 
-  $form.append($back).append($continue); 
+  $form.append($details).append($back).append($continue); 
   return $form;
 }
